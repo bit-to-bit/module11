e0638dfe06743491273a8acab9a19cc4ea555f6b
bit-to-bit <vasiliy.pykhteev@gmail.com>
2023-02-19 23:14:31 +0200

    Second commit

--- src/main/java/StreamUtils.java
+++ src/main/java/StreamUtils.java
@@ -1,22 +1,96 @@
-import java.awt.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class OddPositionNames {
+public class StreamUtils {
 
-    public static String getOddPositionNames (List names) {
+    // Task 1-1: return only odd position names
+    public static String getOddPositionNames1 (List<String> names) {
 
-        List oddNames = new ArrayList();
+        List<String> oddNames = new ArrayList<>();
 
         for (int i = 1; i < names.size(); i += 2) {
             oddNames.add(i + "." + names.get(i));
         }
 
-        return oddNames.stream().collect(Collectors.toList()).toString().replaceAll("(\\[)|(\\])","");
+        return oddNames
+                .stream()
+                .collect(Collectors.toList())
+                .toString().replaceAll("(\\[)|(\\])","");
     }
 
+    // Task 1-2: return only odd position names
+    public static String getOddPositionNames2 (List<String> names) {
+        AtomicInteger i =  new AtomicInteger();
+        return names.stream()
+                .map(x -> {
+                    int n = i.getAndIncrement();
+                    return  n%2 == 1 ? n + "." + x.toString(): null;})
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList())
+                .toString()
+                .replaceAll("(\\[)|(\\])","");
+    }
+
+    // Task 1-3: return only odd position names
+    public static String getOddPositionNames3 (List<String> names) {
+
+        Stream<Integer> counter = Stream.iterate(0, x -> x+1).limit(names.size());
+        Iterator<String> namesIterator =  names.stream().iterator();
+
+        return counter
+                .map(i -> i % 2 == 1 ? i + "." + namesIterator.next(): namesIterator.next().substring(0,0))
+                .filter(name -> !name.equals(""))
+                .collect(Collectors.toList())
+                .toString()
+                .replaceAll("(\\[)|(\\])","");
+    }
+
+    // Task 2: sort names in uppercase
+    public static List<String> sortNames(List<String> names) {
+        return names
+                .stream()
+                .map(String::toUpperCase)
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+    }
+
+    // Task 3: cast array to sort numbers string
+    public static String arrayStringToSortNumbers(String[] arrayOfNumbers) {
+        Stream<String> resultStream = Arrays.stream(arrayOfNumbers);
+        return resultStream
+                .map(s -> s.split(", "))
+                .flatMap(Arrays::stream)
+                .sorted()
+                .collect(Collectors.toList())
+                .toString()
+                .replaceAll("(\\[)|(])","");
+    }
+
+    // Task 4: linear congruence generator
+    public static Stream<Long> linearCongruenceGenerator(Long a, Long c, Long m) {
+        return Stream.iterate(0L, x -> (a * x + c) % m);
+    }
+
+    // Task 5-1: stream mixer: variant 1
+    public static <T> Stream<T> zip(Stream<T> first, Stream<T> second) {
+
+        List<T> result = new ArrayList<>();
+        Iterator<T> firstIterator = first.iterator();
+        Iterator<T> secondIterator = second.iterator();
+        while (firstIterator.hasNext() && secondIterator.hasNext()) {
+            result.add(firstIterator.next());
+            result.add(secondIterator.next());
+        }
+        return result.stream();
+    }
+
+    // Task 5-2: stream mixer: : variant 2
+    public static <T> Stream<T> zip2(Stream<T> first, Stream<T> second) {
+
+        Iterator<T> secondIterator = second.iterator();
+        return first.flatMap(firstValue -> secondIterator.hasNext()?Stream.of(firstValue, secondIterator.next()):Stream.empty());
+
+    }
 }
--- src/main/java/TestStreamUtils.java
+++ src/main/java/TestStreamUtils.java
@@ -1,17 +1,69 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 
 public class TestStreamUtils {
     public static void main(String[] args) {
 
-        // Test OddPositionNames()
+        // Task 1-1: test getOddPositionNames()
         List namesTest0 = new ArrayList<>();
-        List namesTest1 = new ArrayList<>(List.of("Petro","Ivan","Katty","Bill","Sam","Jon","Bruce","Oleg"));
-        System.out.println("OddPositionNames.getOddPositionNames(namesTest0) = " + StreamUtils.getOddPositionNames(namesTest0));
-        System.out.println("OddPositionNames.getOddPositionNames(namesTest1) = " + StreamUtils.getOddPositionNames(namesTest1));
+        List namesTest1 = new ArrayList<>(List.of("Petro","Slava","Katty","Bill","Sam","Jon","Bruce","Oleg"));
 
+        System.out.println("\nTask 1-1: test getOddPositionNames():\n->");
+        System.out.println("StreamUtils.getOddPositionNames1(namesTest0) = " + StreamUtils.getOddPositionNames1(namesTest0));
+        System.out.println("StreamUtils.getOddPositionNames1(namesTest1) = " + StreamUtils.getOddPositionNames1(namesTest1));
+
+        // Task 1-2: test getOddPositionNames2()
+        System.out.println("\nTask 1-2: test getOddPositionNames2():\n->");
+        System.out.println("StreamUtils.getOddPositionNames2(namesTest0) = " + StreamUtils.getOddPositionNames2(namesTest0));
+        System.out.println("StreamUtils.getOddPositionNames2(namesTest1) = " + StreamUtils.getOddPositionNames2(namesTest1));
+
+        // Task 1-3: test getOddPositionNames2()
+        System.out.println("\nTask 1-3: test getOddPositionNames3():\n->");
+        System.out.println("StreamUtils.getOddPositionNames3(namesTest0) = " + StreamUtils.getOddPositionNames3(namesTest0));
+        System.out.println("StreamUtils.getOddPositionNames3(namesTest1) = " + StreamUtils.getOddPositionNames3(namesTest1));
+
+        // Task 2: test sortNames()
+        System.out.println("\nTask 2: test sortNames():\n->");
+        System.out.println("StreamUtils.sortNames(namesTest0) = " + StreamUtils.sortNames(namesTest1));
+
+        // Task 3: test arrayStringToSortNumbers()
+        System.out.println("\nTask 3: test arrayStringToSortNumbers():\n->");
+        String[] numbersTest2 = {"1, 2, 0", "4, 5"};
+        System.out.println("StreamUtils.arrayStringToSortNumbers(numbersTest2) = " + StreamUtils.arrayStringToSortNumbers(numbersTest2));
+
+        // Task 4: linear congruence generator
+        System.out.println("\nTask 4: test linearCongruenceGenerator():\n->");
+        System.out.println("StreamUtils.linearCongruenceGenerator(6L, 2L, 77777777777L) = " + StreamUtils.linearCongruenceGenerator(6L, 2L, 77777777777L).limit(10).collect(Collectors.toList()));
+        System.out.println("StreamUtils.linearCongruenceGenerator(1L, 1L, -9L) = " + StreamUtils.linearCongruenceGenerator(1L, 1L, -9L).limit(1000).collect(Collectors.toList()));
+        System.out.println("StreamUtils.linearCongruenceGenerator(25214903917, 11, 2^48) = " + StreamUtils.linearCongruenceGenerator(25214903917l, 11l, 2 ^ 48l).limit(1000).collect(Collectors.toList()));
+
+
+        // Task 5-1: stream mixer
+        System.out.println("\nTask 5-1: test zip(): \n->");
+
+        Stream<String> streamX1 = Stream.of("!", "@", "#", "$", "%", "^", "&");
+        Stream<String> streamX2 = Stream.of("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11");
+        System.out.println("StreamUtils.zip(streamX1,streamX2) = " + StreamUtils.zip(streamX1, streamX2).collect(Collectors.toList()));
+
+
+        Stream<Integer> streamY1 = Stream.of(90,80,70,60,50,40,30,20,10,0,-10,-20,-30);
+        Stream<Integer> streamY2 = Stream.of(1,2,3,4,5,6,7,8,9);
+        System.out.println("StreamUtils.zip(streamY1,streamY2) = " + StreamUtils.zip(streamY1, streamY2).collect(Collectors.toList()));
+
+        // Task 5-2: stream mixer
+        System.out.println("\nTask 5-2: test zip2(): \n->");
+
+        Stream<String> streamZ1 = Stream.of("!", "@", "#", "$", "%", "^", "&");
+        Stream<String> streamZ2 = Stream.of("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11");
+        System.out.println("StreamUtils.zip2(streamZ1,streamZ2) = " + StreamUtils.zip2(streamZ1, streamZ2).collect(Collectors.toList()));
+
+        Stream<Integer> streamR2 = Stream.of(1,2,3,4,5,6,7,8,9);
+        Stream<Integer> streamR1 = Stream.of(90,80,70,60,50,40,30,20,10,0,-10,-20,-30);
+        System.out.println("StreamUtils.zip2(streamR1,streamR2) = " + StreamUtils.zip2(streamR1, streamR2).collect(Collectors.toList()));
 
     }
-
 
 }
